639319ee5fc61fc407ce7f8740522bf0ac1ccad4
divyanjali0 <navoda@explorevacations.lk>
2025-10-28 09:41:12 +0530

    footer added

---
+++ footer.php
@@ -0,0 +1,88 @@
+
+<footer class="ftco-footer ftco-bg-dark ftco-section">
+	<div class="container">
+	  <div class="row mb-5">
+		<div class="col-md">
+		  <div class="ftco-footer-widget mb-4">
+			<img src="assets/images/logo.png" alt="CarBook Logo" height="100">
+			<p style="text-align: justify; margin-top: 8%;">Platinum Drive Rent a Car offers luxury vehicle rentals for weddings, special events, and corporate travel. With a fleet of high-end cars and professional chauffeurs, we ensure a stylish, comfortable, and memorable experience.</p>
+		 
+		  </div>
+		</div>
+	
+		  <div class="col-md">
+			<div class="ftco-footer-widget mb-4">
+				<!-- <h2 class="ftco-heading-2">We are Located</h2>
+			
+					<iframe 
+					  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7405.353880602396!2d8.51228779204323!3d47.511205671410366!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4790743bf365baf5%3A0xe35763ebf24c7305!2sHofstrasse%2015%2C%208181%20H%C3%B6ri%2C%20Switzerland!5e0!3m2!1sen!2slk!4v1754470128639!5m2!1sen!2slk" 
+					  width="100%" height="200" style="border: 2px solid #ffffff;" allowfullscreen="" loading="lazy"></iframe> -->
+				
+			</div>
+		  </div>
+		<div class="col-md">
+		  <div class="ftco-footer-widget mb-4">
+			  <h2 class="ftco-heading-2">Contact Us</h2>
+			  <div class="block-23 mb-3">
+					<ul>
+				   <li><a href="https://maps.app.goo.gl/xrmbXQFwX8j4buQh6"><span class="icon icon-map-marker"></span><span class="text">No : 375 / 2 / 1, Seeduwa, Sri Lanka</span></a></li> 
+				  <li><a href="tel:+94768200099"><span class="icon icon-phone"></span><span class="text">+94 76 820 0099</span></a></li>
+				  <li><a href="mailto:info@platinumdrive.lk"><span class="icon icon-envelope"></span><span class="text">info@platinumdrive.lk</span></a></li>
+				</ul>
+			  </div>
+			<ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-2">
+			  <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+			  <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+			  <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+			</ul>
+		  </div>
+		</div>
+	  </div>
+	  <div class="row">
+		<div class="col-md-12 text-center">
+
+		  <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All Rights Reserved | Platinum Drive Rent A Car </a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+		</div>
+	  </div>
+	</div>
+  </footer>
+  
+
+
+<!-- loader -->
+<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/jquery-migrate-3.0.1.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/jquery.easing.1.3.js"></script>
+<script src="assets/js/jquery.waypoints.min.js"></script>
+<script src="assets/js/jquery.stellar.min.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<script src="assets/js/aos.js"></script>
+<script src="assets/js/jquery.animateNumber.min.js"></script>
+<script src="assets/js/bootstrap-datepicker.js"></script>
+<script src="assets/js/jquery.timepicker.min.js"></script>
+<script src="assets/js/scrollax.min.js"></script>
+
+<script src="assets/js/google-map.js"></script>
+<script src="assets/js/main.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAHmbwBrk0OKY0Nhp9FrR_zn8HKLGZ54OU&libraries=places"></script>
+  
+  <script>
+    function initAutocomplete() {
+        const pickupInput = document.getElementById('pickup_location');
+        const dropoffInput = document.getElementById('dropoff_location');
+
+        const pickupAutocomplete = new google.maps.places.Autocomplete(pickupInput);
+        const dropoffAutocomplete = new google.maps.places.Autocomplete(dropoffInput);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initAutocomplete);
+</script>
+
