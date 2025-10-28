f32c64667421ea4c105d59aeeba2a7d98ae9161d
divyanjali0 <navoda@explorevacations.lk>
2025-10-28 13:07:03 +0530

    fleet added

---
+++ fleet.php
@@ -0,0 +1,107 @@
+
+    
+    <section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('assets/images/bg_3.jpg');" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+		  <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-start">
+			<div class="col-md-9 ftco-animate pb-5">
+				<p class="breadcrumbs"><span class="mr-2"><a href="[[~1]]">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Cars <i class="ion-ios-arrow-forward"></i></span></p>
+			  <h1 class="mb-3 bread">Choose Your Car</h1>
+			</div>
+		  </div>
+		</div>
+	  </section>
+		  
+  
+		  <section class="ftco-section bg-light">
+		  <div class="container">
+			  <div class="row">
+				  <div class="col-md-4">
+					  <div class="car-wrap rounded ftco-animate">
+						  <div class="img rounded d-flex align-items-end" style="background-image: url(assets/images/fleet/3.jpg);">
+						  </div>
+						  <div class="text">
+							  <h2 class="mb-0" style="color: #102c4e !important;">Mini Cars</h2>
+							  <div class="d-flex mb-3">
+								  <span class="cat">Suzuki WagonR or Similar</span>
+								  <p class="price ml-auto">$ 45 <span>/day</span></p>
+							  </div>
+							  <p class="d-flex mb-0 d-block"><a href="[[~4]]" class="btn btn-primary py-2 mr-1">Book now</a> <a href="[[~4]]" class="btn btn-secondary py-2 ml-1">Details</a></p>
+						  </div>
+					  </div>
+				  </div>
+				  <div class="col-md-4">
+					<div class="car-wrap rounded ftco-animate">
+						<div class="img rounded d-flex align-items-end" style="background-image: url(assets/images/fleet/1.jpg);">
+						</div>
+						<div class="text">
+							<h2 class="mb-0" style="color: #102c4e !important;">Compact Cars</h2>
+							<div class="d-flex mb-3">
+								<span class="cat">Toyota Vitz or Similar</span>
+								<p class="price ml-auto">$ 55 <span>/day</span></p>
+							</div>
+							<p class="d-flex mb-0 d-block"><a href="[[~5]]" class="btn btn-primary py-2 mr-1">Book now</a> <a href="[[~5]]" class="btn btn-secondary py-2 ml-1">Details</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="car-wrap rounded ftco-animate">
+						<div class="img rounded d-flex align-items-end" style="background-image: url(assets/images/fleet/6.jpg);">
+						</div>
+						<div class="text">
+							<h2 class="mb-0" style="color: #102c4e !important;">Standard Cars</h2>
+							<div class="d-flex mb-3">
+								<span class="cat">Honda Grace or Similar</span>
+								<p class="price ml-auto">$ 85 <span>/day</span></p>
+							</div>
+							<p class="d-flex mb-0 d-block"><a href="[[~6]]" class="btn btn-primary py-2 mr-1">Book now</a> <a href="[[~6]]" class="btn btn-secondary py-2 ml-1">Details</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="car-wrap rounded ftco-animate">
+						<div class="img rounded d-flex align-items-end" style="background-image: url(assets/images/fleet/4.jpg);">
+						</div>
+						<div class="text">
+							<h2 class="mb-0" style="color: #102c4e !important;">Premium Cars</h2>
+							<div class="d-flex mb-3">
+								<span class="cat">Honda Civic or Similar</span>
+								<p class="price ml-auto">$ 100 <span>/day</span></p>
+							</div>
+							<p class="d-flex mb-0 d-block"><a href="[[~7]]" class="btn btn-primary py-2 mr-1">Book now</a> <a href="[[~7]]" class="btn btn-secondary py-2 ml-1">Details</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="car-wrap rounded ftco-animate">
+						<div class="img rounded d-flex align-items-end" style="background-image: url(assets/images/fleet/5.jpg);">
+						</div>
+						<div class="text">
+							<h2 class="mb-0" style="color: #102c4e !important;">Sport Utility Vehicle</h2>
+							<div class="d-flex mb-3">
+								<span class="cat">Toyota C-HR or Similar</span>
+								<p class="price ml-auto">$ 110 <span>/day</span></p>
+							</div>
+							<p class="d-flex mb-0 d-block"><a href="[[~8]]" class="btn btn-primary py-2 mr-1">Book now</a> <a href="[[~8]]" class="btn btn-secondary py-2 ml-1">Details</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="car-wrap rounded ftco-animate">
+						<div class="img rounded d-flex align-items-end" style="background-image: url(assets/images/fleet/2.jpg);">
+						</div>
+						<div class="text">
+							<h2 class="mb-0" style="color: #102c4e !important;">Luxury Cars</h2>
+							<div class="d-flex mb-3">
+								<span class="cat">Mercedez Benz or Similar</span>
+								<p class="price ml-auto">$ 210 <span>/day</span></p>
+							</div>
+							<p class="d-flex mb-0 d-block"><a href="[[~9]]" class="btn btn-primary py-2 mr-1">Book now</a> <a href="[[~9]]" class="btn btn-secondary py-2 ml-1">Details</a></p>
+						</div>
+					</div>
+				</div>
+  
+			  </div>
+		
+		  </div>
+	  </section>
